b7b5b367bc83825e76155992b961189571102a72
hasiniainafenovolantsoa-debug <hasiniainafenovolantsoa@gmail.com>
2026-07-07 13:37:59 +0300

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,9 +3,10 @@ const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
-      converter: "dummy",
+      converter: "edge", // Izao no ovaina ho "edge"
     },
   },
 };
 
 export default config;
+
